545b9dd8c874d3f2d03305a6579feb9c2cfd753f
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-02-25 18:15:49 +1300

    Users

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
 
 class App extends Component {
 
@@ -9,7 +9,9 @@ class App extends Component {
     return (
       <div className="App">
         <Navbar />
-        <UserItem />
+        <div className="container">
+          <Users/>
+        </div>
       </div>
     );
   }
--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,14 +1,8 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class UserItem extends Component {
-    state = {
-        id:'id',
-        login:'mojombo',
-        avatar_url:'https://avatars0.githubusercontent.com/u/1?v=4',
-        html_url:'https://github.com/mojombo',
-    }
-    render() {
-        const {login, avatar_url, html_url} = this.state;
+const UserItem = ({user: {login, avatar_url, html_url}}) => {
+
         return (
             <div className="card text-center">
                 <img 
@@ -22,8 +16,11 @@ class UserItem extends Component {
                         More</a>
                 </div>
             </div>
-        )
-    }
+        );
+}
+
+UserItem.propTypes = {
+    user: PropTypes.object.isRequired,
 }
 
 export default UserItem
