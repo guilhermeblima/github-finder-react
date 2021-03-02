c3eb4887f214609a8c33df0b622d89a93794ddd3
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-02 16:51:53 +1300

    react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import axios from 'axios';
-import React, {Component} from 'react';
+import React, {Component, Fragment} from 'react';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
+import About from './components/pages/About';
 import Search from './components/users/Search';
 import Users from './components/users/Users';
 
@@ -37,18 +39,28 @@ class App extends Component {
   render(){
     const {users, loading} = this.state;
     return (
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={this.state.alert}/>
-          <Search 
-            searchUsers={this.searchUsers} 
-            clearUsers={this.clearUsers} 
-            showClearBtn={users.length > 0 ? true : false}
-            setAlert={this.setAlert}/>
-          <Users loading={loading} users={users}/>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <div className="container">
+            <Alert alert={this.state.alert}/>
+            <Switch>
+              <Route exact path='/' render={props => (
+                <Fragment>
+                  <Search 
+                    searchUsers={this.searchUsers} 
+                    clearUsers={this.clearUsers} 
+                    showClearBtn={users.length > 0 ? true : false}
+                    setAlert={this.setAlert}/>
+                  <Users loading={loading} users={users}/>
+                </Fragment>
+              )}/>
+              <Route exact path='/about' component={About}/>
+            </Switch>
+            
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
