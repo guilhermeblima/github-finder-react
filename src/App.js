7bc623fec6ed8d2b7edd89074f011f22ec3db9d8
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-04 19:28:37 +1300

    create home and notfound plus  app.js refactor

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, { Fragment} from 'react';
+import React from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 import Alert from './components/layout/Alert';
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
-import Search from './components/users/Search';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
 import User from './components/users/User';
-import Users from './components/users/Users';
 import AlertState from './context/alert/AlertState';
 
 import GithubState from './context/github/GithubState'
@@ -21,21 +21,15 @@ const App = () => {
             <div className="container">
               <Alert/>
               <Switch>
-                <Route exact path='/' render={props => (
-                  <Fragment>
-                    <Search/>
-                    <Users/>
-                  </Fragment>
-                )}/>
+                <Route exact path='/' component={Home}/>
                 <Route exact path='/about' component={About}/>
                 <Route exact path='/user/:login' component={User} />
+                <Route component={NotFound} />
               </Switch>
-              
             </div>
           </div>
         </Router>
       </AlertState>
-
     </GithubState>
   );
 };
