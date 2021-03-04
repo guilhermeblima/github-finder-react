c6096493af59246a82804df247db0b2e4cd6d129
Guilherme Lima <guilhermelima.iba@gmail.com>
2021-03-04 21:07:11 +1300

    v1.0.1

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -5,7 +5,7 @@ const About = () => {
         <Fragment>
             <h1>About this App</h1>
             <p>App to search Github users</p>
-            <p>Version: 1.0.0</p>
+            <p>Version: 1.0.1</p>
         </Fragment>
     )
 }
